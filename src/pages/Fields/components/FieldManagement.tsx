2e7a588df69172636f16590a7182354000eb1249
mczoff <mczoff@mail.ru>
2025-11-09 17:58:48 +0300

    stage

--- src/pages/Fields/components/FieldManagement.tsx
+++ src/pages/Fields/components/FieldManagement.tsx
@@ -1,54 +1,52 @@
-import { Stack, Text, Flex, Button } from "@mantine/core";
-import { IconPlus } from "@tabler/icons-react";
-import { Field } from "../../../features/Map/model/types";
+import {Stack, Text, Flex, Button, Box} from "@mantine/core";
+import {IconPlus} from "@tabler/icons-react";
+import {Field} from "../../../features/Map/model/types";
 import FieldTemplate from "./FieldTemplate";
 
 export default function FieldManagement({
-  onAddField,
-  data,
-  onFieldSelect,
-  selectedFieldId,
-}: {
-  onAddField: () => void;
-  data: Field[];
-  onFieldSelect: (id: number | undefined) => void;
-  selectedFieldId: number | undefined;
+                                            onAddField,
+                                            data,
+                                            onFieldSelect,
+                                            selectedFieldId,
+                                        }: {
+    onAddField: () => void;
+    data: Field[];
+    onFieldSelect: (id: number | undefined) => void;
+    selectedFieldId: number | undefined;
 }) {
-  return (
-    <Stack
-      mih="100%"
-      gap={0}
-      w={400}
-    >
-      <Flex
-        justify="space-between"
-        p={16}
-        style={{ borderBottom: "1px solid var(--white-gray)" }}
-      >
-        <Text fw={500} fz={18}>
-          Управление полями
-        </Text>
-      </Flex>
-      <Button
-        color="var(--main-color)"
-        m={16}
-        onClick={onAddField}
-        radius="8px"
-      >
-        <Flex>
-          <IconPlus /> <Text>Добавить поле</Text>
-        </Flex>
-      </Button>
-      <Flex gap={8} px={16} direction="column">
-        {data.map((el) => (
-          <FieldTemplate
-            key={el.id}
-            data={el}
-            isSelected={selectedFieldId === el.id}
-            onSelect={() => onFieldSelect(el?.id)}
-          />
-        ))}
-      </Flex>
-    </Stack>
-  );
+    return (
+        <Stack
+            mih="100%"
+            gap={0}
+            w={400}
+        >
+            <Flex
+                justify="space-between"
+                p={16}
+                style={{borderBottom: "1px solid var(--white-gray)"}}
+            >
+                <Text fw={500} fz={18}>
+                    Управление полями
+                </Text>
+            </Flex>
+            <Button
+                color="primary.4"
+                m={16}
+                onClick={onAddField}
+            >
+                <Box mr={8}><IconPlus/></Box>
+                Добавить поле
+            </Button>
+            <Flex gap={8} px={16} direction="column">
+                {data.map((el) => (
+                    <FieldTemplate
+                        key={el.id}
+                        data={el}
+                        isSelected={selectedFieldId === el.id}
+                        onSelect={() => onFieldSelect(el?.id)}
+                    />
+                ))}
+            </Flex>
+        </Stack>
+    );
 }
--- src/pages/Fields/components/FieldViewer.tsx
+++ src/pages/Fields/components/FieldViewer.tsx
@@ -1,94 +1,94 @@
-import { Box, Text, Flex, Button } from "@mantine/core";
-import { useRef } from "react";
+import {Box, Text, Flex, Button} from "@mantine/core";
+import {useRef} from "react";
 import {
-  AgroHubMap,
-  AgroHubMapHandle,
+    AgroHubMap,
+    AgroHubMapHandle,
 } from "../../../features/Map/ui/AgroHubMap";
-import { Field } from "../../../features/Map/model/types";
-import { IconPlus } from "@tabler/icons-react";
+import {Field} from "../../../features/Map/model/types";
+import {IconPlus} from "@tabler/icons-react";
 
 export default function FieldViewer({
-  data,
-  isDrawing,
-  onDrawingComplete,
-  onCancelDrawing,
-  selectedFieldId,
-}: {
-  data: Field[];
-  isDrawing: boolean;
-  onDrawingComplete: (field: Field) => void;
-  onCancelDrawing: () => void;
-  selectedFieldId: number | undefined;
+                                        data,
+                                        isDrawing,
+                                        onDrawingComplete,
+                                        onCancelDrawing,
+                                        selectedFieldId,
+                                    }: {
+    data: Field[];
+    isDrawing: boolean;
+    onDrawingComplete: (field: Field) => void;
+    onCancelDrawing: () => void;
+    selectedFieldId: number | undefined;
 }) {
-  const mapRef = useRef<AgroHubMapHandle>(null);
+    const mapRef = useRef<AgroHubMapHandle>(null);
 
-  return (
-    <Box h={"100%"}>
-      <Box h={"100%"} bdrs={8} w="100%">
-        {selectedFieldId && (
-          <Flex align="center" justify="space-between" p={16}>
-            <Box>
-              <Text fw={500} fz={18}>
-                Поле: Центральное
-              </Text>
-              <Text fw={400} c="var(--subtitle)" fz={12}>
-                Центральная поле ростовской области
-              </Text>
+    return (
+        <Box h={"100%"}>
+            <Box h={"100%"} bdrs={8} w="100%">
+                {selectedFieldId && (
+                    <Flex align="center" justify="space-between" p={16}>
+                        <Box>
+                            <Text fw={500} fz={18}>
+                                Поле: Центральное
+                            </Text>
+                            <Text fw={400} c="var(--subtitle)" fz={12}>
+                                Центральная поле ростовской области
+                            </Text>
+                        </Box>
+
+                        <Button color="primary.4" radius="8px" align={"center"}>
+                            <Box mr={8}><IconPlus/></Box>
+                            Добавить зону
+                        </Button>
+                    </Flex>
+                )}
+
+                {!selectedFieldId && (
+                    <Flex mah={78.5} align="center" justify="space-between" p={16}>
+                        <Box>
+                            <Text fw={500} fz={18}>
+                                Выберите поле
+                            </Text>
+                            <Text fw={400} c="var(--subtitle)" fz={12}>
+                                Выберите поле что бы посмотреть информацию о нем
+                            </Text>
+                        </Box>
+                    </Flex>
+                )}
+
+                <Flex flex={1} h={"calc(100% - 78.5px)"} gap={20}>
+                    <Box p={20} h={"100%"} w={selectedFieldId ? "80%" : "100%"}>
+                        <AgroHubMap
+                            ref={mapRef}
+                            data={data}
+                            isDrawing={isDrawing}
+                            onDrawingComplete={onDrawingComplete}
+                            onCancelDrawing={onCancelDrawing}
+                            selectedFieldId={selectedFieldId}
+                        />
+                    </Box>
+                    {selectedFieldId && (
+                        <Box
+                            style={{
+                                borderRadius: "8px 0px 8px 0px",
+                                borderBottom: "0",
+                                borderRight: "0",
+                            }}
+                            w="20%"
+                            bd={"1px solid var(--white-gray)"}
+                        >
+                            <Text
+                                fw={500}
+                                fz={18}
+                                p={16}
+                                style={{borderBottom: "1px solid var(--white-gray)"}}
+                            >
+                                Зоны поля
+                            </Text>
+                        </Box>
+                    )}
+                </Flex>
             </Box>
-
-            <Button color="var(--main-color)" radius="8px">
-              <IconPlus />
-              <Text>Добавить зону</Text>
-            </Button>
-          </Flex>
-        )}
-
-        {!selectedFieldId && (
-          <Flex mah={78.5} align="center" justify="space-between" p={16}>
-            <Box>
-              <Text fw={500} fz={18}>
-                Выберите поле
-              </Text>
-              <Text fw={400} c="var(--subtitle)" fz={12}>
-                Выберите поле что бы посмотреть информацию о нем
-              </Text>
-            </Box>
-          </Flex>
-        )}
-
-        <Flex flex={1} h={"calc(100% - 78.5px)"} gap={20}>
-          <Box p={20} h={"100%"} w={selectedFieldId ? "80%" : "100%"}>
-            <AgroHubMap
-              ref={mapRef}
-              data={data}
-              isDrawing={isDrawing}
-              onDrawingComplete={onDrawingComplete}
-              onCancelDrawing={onCancelDrawing}
-              selectedFieldId={selectedFieldId}
-            />
-          </Box>
-          {selectedFieldId && (
-            <Box
-              style={{
-                borderRadius: "8px 0px 8px 0px",
-                borderBottom: "0",
-                borderRight: "0",
-              }}
-              w="20%"
-              bd={"1px solid var(--white-gray)"}
-            >
-              <Text
-                fw={500}
-                fz={18}
-                p={16}
-                style={{ borderBottom: "1px solid var(--white-gray)" }}
-              >
-                Зоны поля
-              </Text>
-            </Box>
-          )}
-        </Flex>
-      </Box>
-    </Box>
-  );
+        </Box>
+    );
 }
